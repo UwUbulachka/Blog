997b03dfcbabb25702c380ba13f08ccfd869b118
UwUbulachka <meuw25@gmail.com>
2021-07-04 15:16:50 +0400

    Added device

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+  devise_for :users
+   root to: "home#index"
+
   resource :contacts, only: [:new, :create], path_names: {:new => ''} 
 
   resources :articles do
