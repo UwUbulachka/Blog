42b2bb1205dd8d52a96927441a55e83dd3ca83d8
UwUbulachka <meuw25@gmail.com>
2021-07-14 15:32:37 +0400

    Creating a "subject" and "last_comment" method

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,12 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :text, presence: true
   has_many :comments, dependent: :delete_all
+ 
+  def subject
+    title
+  end
+  
+  def last_comment
+    comments.last
+  end
 end
