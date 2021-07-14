a4e3ed43c21ca74f8c993b80a6f059a01e94d65c
UwUbulachka <meuw25@gmail.com>
2021-07-14 15:52:16 +0400

    Method tests "subject" and "last_comment"

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -11,4 +11,22 @@ describe Article do
     it {should have_many :comments}
   end
 
+  describe "#subject" do
+    it "returns the article title" do
+      article = create(:article, title: 'Foo Bar')
+
+      expect(article.subject).to  eq 'Foo Bar'
+    end
+  end
+
+  describe "#last_comment" do
+    it "returns the last comment" do
+      # создаём статью с 3 комментариями
+      article = create(:article_with_comments)
+
+      # проверка
+      expect(article.last_comment.body).to eq "Comment body 3"
+    end
+  end
+
 end  
